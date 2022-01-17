b6ae493c3842e0976215effd25554b27539b573d
emiliamam <87708280+emiliamam@users.noreply.github.com>
2022-01-17 09:25:11 +0300

    Add files via upload

---
+++ tinkoff_6.py
@@ -0,0 +1,54 @@
+import pygame as p
+import random
+from pygame.locals import *
+
+BLACK = (0 , 0 , 0)
+GREEN = (8, 255, 32)
+
+root = p.display.set_mode((500 , 500))
+cells = [[random.choice([0 , 1]) for j in range(root.get_width() // 20)] for i in range(root.get_height() // 20)]
+
+# часть 1
+def near(pos: list, system=None):
+    if system is None:
+        system = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+    count = 0
+    for i in system:
+        if cells[(pos[0] + i[0]) % len(cells)][(pos[1] + i[1]) % len(cells[0])]:
+            count += 1
+    return count
+
+
+while True:
+    root.fill(GREEN)
+
+    for i in range(0 , root.get_height() // 20):
+        p.draw.line(root , BLACK , (0 , i * 20) , (root.get_width() , i * 20))
+    for j in range(0 , root.get_width() // 20):
+        p.draw.line(root , BLACK , (j * 20 , 0) , (j * 20 , root.get_height()))
+
+    for i in p.event.get():
+        if i.type == QUIT:
+            quit()
+    # часть 2
+
+    for i in range(0 , len(cells)):
+        for j in range(0 , len(cells[i])):
+            print(cells[i][j],i,j)
+            p.draw.rect(root , (255 * cells[i][j] % 256, 125 , 80) , [i * 20 , j * 20 , 20 , 20])
+
+    p.display.update()
+    cells2 = [[0 for j in range(len(cells[0]))] for i in range(len(cells))]
+    for i in range(len(cells)):
+        for j in range(len(cells[0])):
+            if cells[i][j]:
+                if near([i , j]) not in (2 , 3):
+                    cells2[i][j] = 0
+                    continue
+                cells2[i][j] = 1
+                continue
+            if near([i , j]) == 3:
+                cells2[i][j] = 1
+                continue
+            cells2[i][j] = 0
+    cells = cells2
